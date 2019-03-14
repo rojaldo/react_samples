ca328dcb042292badf9b41c9838e4f9a4d5cd96c
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-03-14 16:15:48 +0100

    apod date picker added

--- src/components/apod.js
+++ src/components/apod.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Apod extends Component {
-    state={data:{}};
+    state = { data: {}, startDate: new Date() };
     componentWillMount() {
         console.log('Monto apod');
         fetch('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo')
@@ -10,15 +12,31 @@ export default class Apod extends Component {
 
     }
 
-    processResult(data){
+    processResult(data) {
         console.log(JSON.stringify(data));
-        this.setState({data: data});
+        this.setState({ data: data });
 
+    }
+
+    handleChange(event){
+        const url = 'https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo&date=' 
+        + event.getFullYear() + '-' + (Number(event.getMonth()) + 1) + '-' + event.getDate();
+        console.log(url);
+        fetch(url)
+        .then(response => response.json())
+        .then(data => this.processResult(data))
     }
     render() {
         return (
             <div>
-                
+
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={(event)=>this.handleChange(event)}
+                    dateFormat="yyyy/MM/dd"
+
+                />
+
                 <div className="jumbotron">
                     <div className="container">
                         <h1>{this.state.data.title}</h1>
@@ -29,7 +47,7 @@ export default class Apod extends Component {
                         </p>
                     </div>
                 </div>
-                
+
             </div>
         )
     }
