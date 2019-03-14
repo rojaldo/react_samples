1412bf460f11e96efd4906e474775a9dcc9e2f1c
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-03-14 19:45:55 +0100

    Trivial first version

--- src/components/trivial.js
+++ src/components/trivial.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import { Card } from '../model/card';
 
 export default class Trivial extends Component {
-    state = { data: {} };
+    state = { data: {}, cards: [] };
     componentWillMount() {
         console.log('Monto apod');
         fetch('https://opentdb.com/api.php?amount=10')
@@ -11,13 +12,42 @@ export default class Trivial extends Component {
     processResult(data) {
         console.log(JSON.stringify(data));
         this.setState({ data: data });
-
+        data.results.forEach(jsonCard => {
+            const card = new Card(jsonCard);
+            this.setState({ cards: [...this.state.cards, card] });
+        });
 
     }
-    render() {
-        return (
-            <div>
 
+    handleClick(indexCard,indexButton) {
+        console.log('card: ' + indexCard);
+        let myCard = this.state.cards[indexCard];
+        myCard.responded = true;
+        let myCards = this.state.cards;
+        myCards.splice(indexCard, 1, myCard);
+        this.setState({cards: myCards});
+    }
+
+    render() {
+        const renderHTML = (rawHTML) => React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+
+        const cards = this.state.cards.map(
+            (card, indexCard) => <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 p-1" key={indexCard}>
+                <div className="card">
+                    <div className="card-body">
+                        <h4 className="card-title">{renderHTML(card.question)}</h4>
+                        {card.answers.map(
+                            (answer, indexButton) => <button type="button" className="btn btn-primary btn-block" key={indexButton} onClick={(indexCard,indexButton)=>this.handleClick(indexCard,indexButton)}>{answer}</button>)
+                        }
+                    </div>
+                </div>
+            </div>);
+
+        return (
+            <div class="container-fluid">
+                <div className="row">
+                    {cards}
+                </div>
             </div>
         )
     }
--- src/model/card.js
+++ src/model/card.js
@@ -1,5 +1,13 @@
 export class Card {
+    question = '';
+    correctAnswer = '';
+    answers = [];
+    responded = false;
+    rightAnswered = false;
+
     constructor(jsonCard){
         this.question = jsonCard.question;
+        this.correctAnswer = jsonCard.correct_answer;
+        this.answers = [...jsonCard.incorrect_answers, jsonCard.correct_answer]
     }
 }
